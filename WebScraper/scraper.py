346d127d4c3565c4242fa51554e0ff4139d218df
laujuu <laurafayec@gmail.com>
2023-04-11 18:38:39 -0300

    feat: Implementa funções para raspagem do MELI e Buscapé

---
+++ WebScraper/scraper.py
@@ -0,0 +1,107 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def search_mercadolivre_by_category(category):
+    url = f"https://lista.mercadolivre.com.br/{category}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    products = soup.find_all("li", {"class": "ui-search-layout__item"})
+    results = []
+    for product in products:
+        title = product.find("h2", {"class": "ui-search-item__title"}).text.strip()
+        price = product.find("span", {"class": "price-tag-fraction"}).text.strip()
+        link = product.find("a", {"class": "ui-search-link"})['href']
+        image = product.find("img")['src']
+        results.append({
+            "title": title,
+            "price": price,
+            "link": link,
+            "image": image,
+            "category": category,
+            "website": "Mercado Livre",
+            "keyword": ""
+        })
+    return results
+
+def search_mercadolivre_by_keyword(category, keyword):
+    url = f"https://lista.mercadolivre.com.br/{category}/{keyword}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    products = soup.find_all("li", {"class": "ui-search-layout__item"})
+    results = []
+    for product in products:
+        title = product.find("h2", {"class": "ui-search-item__title"}).text.strip()
+        price = product.find("span", {"class": "price-tag-fraction"}).text.strip()
+        link = product.find("a", {"class": "ui-search-link"})['href']
+        image = product.find("img")['src']
+        results.append({
+            "title": title,
+            "price": price,
+            "link": link,
+            "image": image,
+            "category": category,
+            "website": "Mercado Livre",
+            "keyword": keyword
+        })
+    return results
+
+
+def search_buscape_by_category(category):
+    url = f"https://www.buscape.com.br/{category}"
+    # fazer request e parsear html
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # obter produtos
+    products = soup.find_all("div", {"class": "SearchCard_ProductCard__1D3ve"})
+
+    results = []
+
+    for product in products:
+        # obter informações do produto
+        title = product.find("h2", {"class": "Text_Text__h_AF6"}).text.strip()
+        price = product.find("p", {"class": "Text_Text__h_AF6"}).text.strip()
+        link = product.find("a", {"class": "SearchCard_ProductCard_Inner__7JhKb"})['href']
+        image = product.find("img")['src']
+
+        results.append({
+            "title": title,
+            "price": price,
+            "link": link,
+            "image": image,
+            "category": category,
+            "website": "Buscapé",
+            "keyword": ""
+        })
+
+    return results
+
+def search_buscape_by_keyword(category, keyword):
+    url = f"https://www.buscape.com.br/{category}/{keyword}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # obter produtos
+    products = soup.find_all("div", {"class": "SearchCard_ProductCard__1D3ve"})
+
+    results = []
+
+    for product in products:
+        # obter informações do produto
+        title = product.find("h2", {"class": "Text_Text__h_AF6"}).text.strip()
+        price = product.find("p", {"class": "Text_Text__h_AF6"}).text.strip()
+        link = product.find("a", {"class": "SearchCard_ProductCard_Inner__7JhKb"})['href']
+        image = product.find("img")['src']
+
+        results.append({
+            "title": title,
+            "price": price,
+            "link": link,
+            "image": image,
+            "category": category,
+            "website": "Buscapé",
+            "keyword": ""
+        })
+
+    return results
